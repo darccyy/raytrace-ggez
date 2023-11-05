c7631ac227c533742dc88e3e7a3bd34cc07c2951
darcy <acednes@gmail.com>
2023-11-06 10:59:19 +1100

    add rectangles

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@ use std::collections::HashSet;
 
 use ggez::{
     event::{EventHandler, MouseButton},
-    graphics::{self, DrawMode, DrawParam, Mesh, Rect, TextLayout},
+    graphics::{self, Color, DrawMode, DrawParam, Mesh, Rect, TextLayout},
     mint::Point2,
     winit::event::VirtualKeyCode,
     Context,
 };
+use rand::Rng;
 
 /// Returns `ggez::graphics::Color` value, as const
 macro_rules! color {
@@ -38,15 +39,42 @@ pub struct App {
     keys_down: HashSet<VirtualKeyCode>,
     camera: Point2<f32>,
     mouse: Point2<f32>,
+    rects: Vec<(Rect, Color)>,
 }
 
 impl App {
-    pub fn new(_ctx: &mut Context) -> Self {
+    pub fn new(ctx: &mut Context) -> Self {
+        let (width, height) = ctx.gfx.size();
+
         let camera = Point2 { x: 100.0, y: 100.0 };
+
+        let mut rects = Vec::new();
+        let mut rng = rand::thread_rng();
+        for _ in 0..10 {
+            let rw = rng.gen_range(10.0..80.0);
+            let rh = rng.gen_range(10.0..80.0);
+
+            let rect = Rect::new(
+                rng.gen_range(0.0..width - rw),
+                rng.gen_range(0.0..height - rh),
+                rw,
+                rh,
+            );
+            let color = color!(
+                rng.gen_range(0..255),
+                rng.gen_range(0..255),
+                rng.gen_range(0..255),
+            );
+            rects.push((rect, color));
+        }
+
+        // let rects = vec![(Rect::new(200.0, 70.0, 30.0, 50.0), color!(MAGENTA))];
+
         Self {
             keys_down: HashSet::new(),
             camera,
             mouse: camera,
+            rects,
         }
     }
     fn reset(&mut self, ctx: &mut Context) {
@@ -57,8 +85,8 @@ impl App {
 impl EventHandler for App {
     fn update(&mut self, _ctx: &mut Context) -> Result<(), ggez::GameError> {
         macro_rules! any_keys {
-            ( $( $key:ident ),* ) => {
-                $( self.keys_down.contains(&VirtualKeyCode::$key) ||)* false
+            ( $( $key:ident ),+ ) => {
+                false $( || self.keys_down.contains(&VirtualKeyCode::$key) )*
             };
         }
 
@@ -82,7 +110,11 @@ impl EventHandler for App {
 
     fn draw(&mut self, ctx: &mut Context) -> Result<(), ggez::GameError> {
         let mut canvas = graphics::Canvas::from_frame(ctx, color!(BLACK));
-        // let (width, height) = ctx.gfx.size();
+
+        for (rect, color) in &self.rects {
+            let mesh = Mesh::new_rectangle(ctx, DrawMode::fill(), *rect, *color)?;
+            canvas.draw(&mesh, DrawParam::default());
+        }
 
         let radius = 10.0;
         let mesh = Mesh::new_circle(ctx, DrawMode::fill(), self.camera, radius, 0.5, color!(RED))?;
